df189727eabda8deaf69fe9aa7cf7eaf25a5aea7
LaloP1 <al222210666@gmail.com>
2023-12-28 20:01:16 -0600

    Validaciones

--- app/Livewire/Formulario.php
+++ app/Livewire/Formulario.php
@@ -6,13 +6,36 @@ use Livewire\Component;
 use App\Models\Category;
 use App\Models\Tag;
 use App\Models\Post;
+use Livewire\Attributes\Rule;
 
 class Formulario extends Component
 {
+    //Definicion de propiedades
     public $categories, $tags;
-    public $category_id = '', $title, $content;
+
+    #[Rule('required', message:'El campo titulo es requerido')]
+    public $title;
+
+    #[Rule('required')]
+    public $content;
+
+    #[Rule ('required', as:'categoria')]
+    public $category_id = '';
+
+    #[Rule ('required|array')]
     public $selectedTags = [];
+
     public $posts;
+
+    public $postEditId = '';
+
+    public $open = false;
+    public $postEdit = [
+        'category_id' => '',
+        'title' => '',
+        'content' => '',
+        'tags' => []
+    ];
 
     public function mount(){
         $this->categories = Category::all();
@@ -20,6 +43,19 @@ class Formulario extends Component
         $this->posts = Post::all();
     }
     public function save(){
+
+        //Crea una validacion para hacer los campos requeridos
+        $this->validate();
+        // $this->validate([
+        //     'title'=>'required',
+        //     'content'=>'required',
+        //     'category_id'=>'required|exists:categories,id',
+        //     'selectedTags'=> 'required|array'
+        // ],[
+        //     'title.required'=>'El campo titulo es requerido',
+        // ],[
+        //     'category_id'=>'categoria',
+        // ]);
 
         // $post = Post::create([
         //     'category_id' => $this->category_id,
@@ -41,6 +77,44 @@ class Formulario extends Component
         //     'content' => $this->content,
         //     'tags' => $this->selectedTags,
         // ]);
+    }
+    public function edit($postId){
+        $this -> open = true;
+
+        //Vamos a asignarle a postEditId el valor que contiene postId
+        $this->postEditId = $postId;
+
+        //busca la info a traves del id para encontrar el post a editar
+        $post = Post::find($postId);
+
+        //Dice que el nuevo dato reemplaza al dato antiguo
+        $this->postEdit['category_id'] = $post->category_id;
+        $this->postEdit['title']= $post->title;
+        $this->postEdit['content'] = $post-> content;
+
+        $this->postEdit['tags'] = $post->tags->pluck('id')->toArray();
+
+    }
+    public function update(){
+        $post = Post::find($this->postEditId);
+        $post->update([
+            'category_id' => $this->postEdit['category_id'],
+            'title' => $this->postEdit['title'],
+            'content' => $this->postEdit['content']
+        ]);
+        $post->tags()->sync($this->postEdit['tags']);
+
+        $this->reset(['postEditId', 'postEdit', 'open']);
+
+        $this->posts = Post::all();
+    }
+    public function destroy($postId){
+        //Busca el id en el Modelo
+        $post = Post::find($postId);
+        //Ejecuta la eliminacion
+        $post->delete();
+        //Refresca los post
+        $this->posts = Post::all();
     }
     public function render()
     {
--- resources/views/livewire/formulario.blade.php
+++ resources/views/livewire/formulario.blade.php
@@ -1,29 +1,29 @@
 <div>
-    <div class="bg-white shadow rounded-lg p-6">
+    <div class="bg-white shadow rounded-lg p-6 mb-[15px]">
         <form wire:submit="save">
             <div class="mb-4">
                 <x-label>
                     Nombre
                 </x-label>
                 <x-input class="w-full"
-                wire:model="title"
-                required />
+                wire:model="title" />
+                <x-input-error for="title"></x-input-error>
             </div>
             <div class="mb-4">
                 <x-label>
                     Contenido
                  </x-label>
                 <x-textarea class="w-full"
-                wire:model="content"
-                required></x-textarea>
+                wire:model="content">
+                </x-textarea>
+                <x-input-error for="content"></x-input-error>
             </div>
             <div class="mb-4 w-full">
                 <x-label>
                     Categorias
                 </x-label>
                     <x-select class="w-full"
-                    wire:model="category_id"
-                    required>
+                    wire:model="category_id">
                         <option value="" disabled>Seleccione una categoria</option>
                         @foreach ($categories as $category )
                         <option value="{{ $category->id }}">
@@ -31,6 +31,7 @@
                         </option>
                         @endforeach
                     </x-select>
+                    <x-input-error for="category_id"></x-input-error>
             </div>
             <div class="mb-4 ">
                 <x-label>
@@ -47,6 +48,7 @@
                         </li>
                     @endforeach
                 </ul>
+                <x-input-error for="selectedTags"></x-input-error>
             </div>
             <div class="flex justify-end">
                 <x-button>
@@ -55,13 +57,95 @@
             </div>
         </form>
     </div>
-    <div class="bg-white shadow rounded-lg p-6">
-        <ul >
+
+    <div class="bg-white shadow rounded-lg p-6 ">
+        <ul class="list-disc list-inside space-y-2">
             @foreach ($posts as $post)
-            <li>
+            <li class="flex justify-between"
+            {{-- se utiliza para optimizar el rendimiento de Livewire al gestionar listas dinámicas y garantizar que Livewire pueda realizar actualizaciones eficientes al seguir los cambios en elementos específicos de la interfaz de usuario--}}
+            wire:key="post-{{ $post->id }}">
                 {{ $post->title }}
+                <div>
+                    <x-button wire:click="edit({{ $post->id }})">
+                        Editar
+                    </x-button>
+                    <x-danger-button wire:click="destroy({{ $post->id }})">
+                        Eliminar
+                    </x-danger-button>
+                </div>
             </li>
             @endforeach
         </ul>
     </div>
+
+    {{-- Formulario de edicion --}}
+    <form wire:submit="update">
+        <x-dialog-modal wire:model="open">
+            <x-slot name="title">
+                Actualizar el Post
+            </x-slot>
+            <x-slot name="content">
+                    <div class="mb-4">
+                        <x-label>
+                            Nombre
+                        </x-label>
+                        <x-input class="w-full"
+                        {{-- Estoy sincronizando el contenido de postEdit con el formulario input --}}
+                        wire:model="postEdit.title"
+                        />
+                    </div>
+                    <div class="mb-4">
+                        <x-label>
+                            Contenido
+                        </x-label>
+                        <x-textarea class="w-full"
+                        wire:model="postEdit.content"
+                        ></x-textarea>
+                    </div>
+                    <div class="mb-4 w-full">
+                        <x-label>
+                            Categorias
+                        </x-label>
+                            <x-select class="w-full"
+                            wire:model="postEdit.category_id">
+                                <option value="" disabled>Seleccione una categoria</option>
+                                @foreach ($categories as $category )
+                                <option value="{{ $category->id }}">
+                                    {{ $category->name }}
+                                </option>
+                                @endforeach
+                            </x-select>
+                    </div>
+                    <div class="mb-4 ">
+                        <x-label>
+                            Etiquetas
+                        </x-label>
+
+                        <ul>
+                            @foreach ($tags as $tag )
+                                <li>
+                                    <label>
+                                        <x-checkbox wire:model="postEdit.tags" value="{{ $tag->id }}" />
+                                        {{ $tag->name }}
+                                    </label>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+            </x-slot>
+            <x-slot name="footer">
+                <div class="flex justify-end">
+                    <x-danger-button
+                    class=" mr-3"
+                    wire:click="$set('open', false)">
+                        Cancelar
+                    </x-danger-button>
+                    <x-button>
+                        Editar
+                    </x-button>
+                </div>
+            </x-slot>
+        </x-dialog-modal>
+    </form>
+
 </div>
